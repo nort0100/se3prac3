823d4f99e64a10433b498df5811f99b160744f67
Stuart <nort0100@flinders.edu.au>
2014-06-18 14:28:45 +0930

    Deleted most of previous work, change of method to be used, to achieve solution

--- tilt.c
+++ tilt.c
@@ -7,21 +7,12 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
-  int i1,i2,i3,i4;//integer variables
-  int list[4] startVector = {i1,i2,i3,i4};
-  int i = 0;
-  for (i < 4)//moving values, only 1 at a time currently, need to change to move if value exists move to furthest left pos available
-  {
-      if (i = 3)
-      {
-          startVector[i] = 0;
-          return;
-      }
-   startVector[i] = startVector[i+1]; 
-   i++;
-  }
 
   // combine tiles as required
 
+  int i1,i2,i3,i4;//integer variables
+  int list[4] startVector = {i1,i2,i3,i4};
+
+  
   return 0;
 }
